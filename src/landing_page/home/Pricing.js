62e591b96b84d576dbb57c755856aee51f8d7656
manoj-kumar-rai <iammanojkumarrai@gmail.com>
2026-05-06 08:17:19 +0530

    Education Section Design is  Done

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 function Pricing() {
   return (
-    <div>Pricing</div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-4'>
+          <h1 className='mb-5'>Unbeatable pricing</h1>
+          <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+          <a href='' style={{textDecoration:"none"}}>See Pricing <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+        </div>
+        <div className='col-2'></div>
+        <div className='col-6 mb-5'>
+          <div className='row'>
+            <div className='col text-center border p-3'>
+              <h1 className='mb-3'><i class="fa fa-inr" aria-hidden="true"></i>0</h1>
+              <p>Free equity delivery and <br/>direct mutual funds</p>
+            </div>
+            <div className='col text-center border p-3'>
+              <h1  className='mb-3'><i class="fa fa-inr" aria-hidden="true"></i>20</h1>
+              <p>Intraday & F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
