5b07f5452b57238ea30588a0008e80889dc3c338
manoj-kumar-rai <iammanojkumarrai@gmail.com>
2026-05-16 14:05:25 +0530

    Set up done of React Router Dom

--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Navbar'
 import Hero from './Hero'
 import Brokerage from './Brokerage'
+import Footer from '../Footer'
 
 function PricingPage() {
   return (
--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Navbar'
 import Hero from '../home/Hero'
 import Footer from '../Footer'
+import RaiseTicket from './RaiseTicket'
 
 function SupportPage() {
   return (
